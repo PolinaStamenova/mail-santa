d4eecca34e0ec9528f0c9748d18e2c60c1ee06bc
Polina Stamenova <polina.stamenova@gmail.com>
2024-10-21 18:20:38 +0300

    Implement basic functionality for writing a letter to Santa
    - Add form for children to submit their letters to Santa.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
     it { should have_many(:child_wishes) }
     it { should have_many(:presents).through(:child_wishes) }
   end
-  
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
