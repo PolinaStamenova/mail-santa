cbb05d8d23099d439d4f99d36e3aa9630a28e028
Polina Stamenova <polina.stamenova@gmail.com>
2024-11-17 14:24:06 +0200

    Fix database:
    Rename the db & remove unnecessary migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_11_101008) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_04_151222) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "child_wishes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
