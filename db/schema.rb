6a414e4858349a80898b6e29542bb94f5416253d
Polina Stamenova <polina.stamenova@gmail.com>
2024-11-21 19:04:51 +0200

    Enable Pgcrypto Extension to use UUID as ID

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_20_083935) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_21_170311) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "child_wishes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
