1e7ef6429f72739623b186f1906fec3e0fc65c10
Polina Stamenova <polina.stamenova@gmail.com>
2024-11-21 20:38:28 +0200

    HOTFIX: cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins 'https://mailsanta-2a6e63705071.herokuapp.com', 'https://myapp.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end
