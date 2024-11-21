d7838ef8058fa744409ec1ae70518ea51568204b
Polina Stamenova <polina.stamenova@gmail.com>
2024-11-21 11:38:02 +0200

    Use uuid for webhooks

--- db/migrate/20241120083935_create_webhooks.rb
+++ db/migrate/20241120083935_create_webhooks.rb
@@ -1,6 +1,6 @@
 class CreateWebhooks < ActiveRecord::Migration[7.0]
   def change
-    create_table :webhooks do |t|
+    create_table :webhooks, id: :uuid do |t|
       t.string :url
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_20_083935) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "webhooks", force: :cascade do |t|
+  create_table "webhooks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
